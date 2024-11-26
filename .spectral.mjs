81b5926024dfbde457a9616d4a79284d1dfda953
Maciej Papież <maciej.papiez+stoplight@calamari.io>
2024-11-26 15:01:47 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2NTM5Mw/spectral.js?branch_slug=main&token=67e54d5b-c2d1-4942-a240-f6449bafc92d";
+export default { extends: ruleset };
